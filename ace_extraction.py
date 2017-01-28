backprop through tagger and fixed boundary eval in extractor

ace_extraction.py
@@ -95,6 +95,7 @@ def train(dataset, tagger,
                           use_mlp=use_mlp,
                           bidirectional=bidirectional,
                           shortcut_embeds=shortcut_embeds,
+                          backprop_to_tagger=backprop,
                           start_tags=start_tags, in_tags=in_tags, out_tags=out_tags,
                           tag2mtype=tag2mtype,
                           mtype2msubtype=mtype2msubtype,
@@ -206,7 +207,7 @@ def train(dataset, tagger,
             # run model
             start = time.time()
             loss = extractor_loss(x_list, m_list, r_list,
-                                  backprop_to_tagger=False)
+                                  backprop_to_tagger=backprop_to_tagger)
             STATS['forward_times'].append(time.time()-start)
             loss_val = np.asscalar(loss.data)
             print_batch_loss(loss_val,
@@ -305,6 +306,7 @@ def parse_args():
     parser.add_argument('--lstm_size',
                         default=50,
                         type=int)
+    parser.add_argument('--backprop', action='store_true', default=False)
     parser.add_argument('--use_mlp', action='store_true', default=False)
     parser.add_argument('--bidirectional', action='store_true', default=False)
     parser.add_argument('--shortcut_embeds', action='store_true', default=False)